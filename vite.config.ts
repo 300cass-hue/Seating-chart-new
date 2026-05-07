4d889b8ecb83c06d4aff0ccef099a8eff68520a1
300cass-hue <300cass@gmail.com>
2026-05-07 13:07:21 -0700

    feat: Configure Vite for correct module resolution
    Updates the Vite configuration to correctly resolve the '@' alias to the './src' directory. This ensures that module imports within the application function as expected.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({mode}) => {
   return {
@@ -9,7 +13,7 @@ export default defineConfig(({mode}) => {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
